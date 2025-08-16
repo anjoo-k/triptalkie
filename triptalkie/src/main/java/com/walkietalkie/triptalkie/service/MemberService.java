51e2ac8e733ba6d7620379ffb83f048ad4276e6a
yey3hh <wateveryehwant@gmail.com>
2025-08-16 20:59:37 +0900

    feat: 아이디, 닉네임, 전화번호, 이메일 중복 확인 버튼 이벤트 추가, 백엔드 기능 구현

--- triptalkie/src/main/java/com/walkietalkie/triptalkie/service/MemberService.java
+++ triptalkie/src/main/java/com/walkietalkie/triptalkie/service/MemberService.java
@@ -2,6 +2,7 @@ package com.walkietalkie.triptalkie.service;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.walkietalkie.triptalkie.domain.Member;
 import com.walkietalkie.triptalkie.mapper.MemberMapper;
@@ -23,6 +24,46 @@ public class MemberService {
 		member.setPassword(hashedPassword);
 		member.setCredit(50.0);
 		return memberMapper.register(member);
+	}
+	
+	/**
+	 * @Transactional(readOnly = true) 
+	 * 위 설정이 명시된 메서드는 트랜잭션 상에서 데이터 변경을 하지 않을 것임을 알림
+	 * ==> 읽기 작업에만 집중하게 하여 성능 향상
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@Transactional(readOnly = true)
+	public int checkMemberById(String id) {
+		if(id == null || id.trim().isEmpty()) {
+			throw new IllegalArgumentException("아이디를 입력하세요");
+		}
+		return memberMapper.checkMemberById(id);
+	}
+	
+	@Transactional(readOnly = true)
+	public int checkMemberByNickname(String nickname) {
+		if (nickname == null || nickname.trim().isEmpty()) {
+			throw new IllegalArgumentException("닉네임을 입력하세요");
+		}
+		return memberMapper.checkMemberByNickname(nickname);
+	}
+
+	@Transactional(readOnly = true)
+	public int checkMemberByPhonenumber(String phonenumber) {
+		if (phonenumber == null || phonenumber.trim().isEmpty()) {
+			throw new IllegalArgumentException("전화번호를 입력하세요");
+		}
+		return memberMapper.checkMemberByPhonenumber(phonenumber);
+	}
+	
+	@Transactional(readOnly = true)
+	public int checkMemberByEmail(String email) {
+		if (email == null || email.trim().isEmpty()) {
+			throw new IllegalArgumentException("이메일을 입력하세요");
+		}
+		return memberMapper.checkMemberByEmail(email);
 	}
 
 }
