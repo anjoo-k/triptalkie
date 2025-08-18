c1f6e4e0dcf522239d31ebe4a41c0158fed26804
yey3hh <wateveryehwant@gmail.com>
2025-08-18 09:45:59 +0900

    fix: 회원가입, 로그인 부분 로직 수정 및 코드 정리

--- triptalkie/src/main/java/com/walkietalkie/triptalkie/controller/MemberController.java
+++ triptalkie/src/main/java/com/walkietalkie/triptalkie/controller/MemberController.java
@@ -1,7 +1,6 @@
 package com.walkietalkie.triptalkie.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,64 +13,69 @@ import com.walkietalkie.triptalkie.service.MemberService;
 @Controller
 @RequestMapping("/member")
 public class MemberController {
-	
+
 	private final MemberService memberService;
-	
+
 	public MemberController(MemberService memberService) {
 		super();
 		this.memberService = memberService;
 	}
 
 	/*
-	 * checkMemberById, checkMemberByNickname, checkMemberByPhonenumber, checkMemberByEmail
-	 * 중복 확인 메서드
+	 * checkMemberById, checkMemberByNickname, checkMemberByPhonenumber,
+	 * checkMemberByEmail 중복 확인 메서드
 	 */
 	@GetMapping("/checkMemberById")
 	@ResponseBody
 	public int checkMemberById(@RequestParam String id) {
 		return memberService.checkMemberById(id);
 	}
-	
+
 	@GetMapping("/checkMemberByNickname")
 	@ResponseBody
 	public int checkMemberByNickname(@RequestParam String nickname) {
 		return memberService.checkMemberByNickname(nickname);
 	}
-	
+
 	@GetMapping("/checkMemberByPhonenumber")
 	@ResponseBody
 	public int checkMemberByPhonenumber(@RequestParam String phonenumber) {
 		return memberService.checkMemberByPhonenumber(phonenumber);
 	}
-	
+
 	@GetMapping("/checkMemberByEmail")
 	@ResponseBody
 	public int checkMemberByEmail(@RequestParam String email) {
 		return memberService.checkMemberByEmail(email);
 	}
-	
+
 	/*
 	 * 회원 가입
 	 */
-	@PostMapping("/registerMember")
-	public String registerMember(Member member, Model model) {
-		System.out.println("registerMember : " + member);
-		memberService.register(member);
-		model.addAttribute("nickname", member.getNickname());
-		
-		return "redirect:/registerMemberFin";
+	@GetMapping("/registerPage")
+	public String registerPage() {
+		return "pages/member/register";
 	}
 	
-	@GetMapping("/registerMemberFin")
-	public String registerMemberFin(Model model) {
-		System.out.println("가입 완");
-
-		return "home";
+	
+	@PostMapping("/register")
+	public String register(Member member) {
+		int result = memberService.register(member);
+		return "redirect:/login";
 	}
 
-	 @PostMapping("/register")
-	  public String register(Member member) {
-		int result = memberService.register(member);
-	    return "redirect:/login";
-	  }
+	/*
+	 * 로그인
+	 */
+	@GetMapping("/loginPage")
+	public String loginPage() {
+		return "pages/member/login";
+	}
+	
+	@PostMapping("/login")
+	public String login() {
+		
+		return "redirect:/home";
+	}
+
 }
