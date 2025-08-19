d81942324605ddcf2798c092bbf0d534c8cc281f
GIS-BM <gogointospace@gmail.com>
2025-08-19 16:22:11 +0900

    여행 정보 게시판 CRUD UI 기능, 백엔드 기능 구현

--- triptalkie/src/main/java/com/walkietalkie/triptalkie/service/TravelInfoService.java
+++ triptalkie/src/main/java/com/walkietalkie/triptalkie/service/TravelInfoService.java
@@ -1,5 +1,6 @@
 package com.walkietalkie.triptalkie.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,19 +10,32 @@ import org.springframework.transaction.annotation.Transactional;
 import com.walkietalkie.triptalkie.domain.TravelInfo;
 import com.walkietalkie.triptalkie.mapper.TravelInfoMapper;
 
+import jakarta.servlet.http.HttpSession;
+
 @Service
 @Transactional
 public class TravelInfoService {
+
+    private final MemberService memberService;
   private final TravelInfoMapper travelInfoMapper;
   
   // [] 의존성 주입
-  public TravelInfoService(TravelInfoMapper travelInfoMapper) {
+  public TravelInfoService(TravelInfoMapper travelInfoMapper, MemberService memberService) {
     this.travelInfoMapper = travelInfoMapper;
+    this.memberService = memberService;
   }
   
   // 여행 정보 글 등록
-  public int registerTravelInfo(TravelInfo travelInfo) {
-    int result = travelInfoMapper.registerTravelInfo(travelInfo);
+  public int registerTravelInfo(TravelInfo travelInfo, HttpSession session) {
+    int result = 0;
+    String currentMemberId = memberService.getLoginId(session);
+    String currentgetMemberNickname = memberService.getLoginNickname(session);
+    
+    // traveInfo 객체에 로그인한 사용자 id, nickname 넣기
+    travelInfo.setMemberId(currentMemberId);
+    travelInfo.setMemberNickname(currentgetMemberNickname);
+    
+    result = travelInfoMapper.registerTravelInfo(travelInfo);
     
     return result;
   }
@@ -33,22 +47,33 @@ public class TravelInfoService {
   }
     
   // 여행 정보 글 idx 기준으로 상세 정보 찾기
-  TravelInfo findTravelInfoIdx(long idx) {
+  public TravelInfo findTravelInfoIdx(long idx) {
+    // 조회수 증가 로직 추가해야 함
+    
     return travelInfoMapper.findTravelInfoIdx(idx);
   }
   
   
-  // 여행정보 글 업데이트
-  int updateTravelInfoByIdxAndMemberId(TravelInfo travelInfo) {
-    int result = travelInfoMapper.updateTravelInfoByIdxAndMemberId(travelInfo);
-    
-    return result;
+  // 여행정보 글 수정
+  @Transactional
+  public int updateTravelInfoByIdxAndMemberId(TravelInfo travelInfo, HttpSession session) {
+    // 세션에서 로그인한 사용자 ID 가져오기
+    String loginId = (String) session.getAttribute("loginId");
+    if (loginId == null || !loginId.equals(travelInfo.getMemberId())) {
+      return 0; // 작성자가 아니면 수정 실패
+    }
+
+    // 수정일자 업데이트
+    travelInfo.setUpdatedAt(LocalDateTime.now());
+
+    return travelInfoMapper.updateTravelInfoByIdxAndMemberId(travelInfo);
   }
   
   
   // 여행정보 글 삭제
   public int deleteTravelInfoByIdx(long idx) {
     int result = travelInfoMapper.deleteTravelInfoByIdx(idx);
+
     
     return result;
   }
