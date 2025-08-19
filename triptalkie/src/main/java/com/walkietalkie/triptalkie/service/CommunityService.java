5ee6cb2fe503e837f00595f3da842381453a0501
beomjunkim-soul <kbg142337@naver.com>
2025-08-19 15:01:54 +0900

    fix: git 충돌 mapper 해결

--- triptalkie/src/main/java/com/walkietalkie/triptalkie/service/CommunityService.java
+++ triptalkie/src/main/java/com/walkietalkie/triptalkie/service/CommunityService.java
@@ -1,7 +1,6 @@
 package com.walkietalkie.triptalkie.service;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.walkietalkie.triptalkie.domain.Community;
 import com.walkietalkie.triptalkie.mapper.CommunityMapper;
