aae4b6b8317ee81a7514aec0f472a252a3ea8f91
GIS-BM <gogointospace@gmail.com>
2025-08-20 12:32:36 +0900

    feature : 정보 기능 개선

--- triptalkie/src/main/java/com/walkietalkie/triptalkie/domain/Member.java
+++ triptalkie/src/main/java/com/walkietalkie/triptalkie/domain/Member.java
@@ -14,135 +14,90 @@ public class Member {
 	private String address;
 	private String travelConcept;
 	private double credit;
+	private boolean active;
+  public Member() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+  public String getId() {
+    return id;
+  }
+  public void setId(String id) {
+    this.id = id;
+  }
+  public String getNickname() {
+    return nickname;
+  }
+  public void setNickname(String nickname) {
+    this.nickname = nickname;
+  }
+  public String getPassword() {
+    return password;
+  }
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public LocalDate getBirth() {
+    return birth;
+  }
+  public void setBirth(LocalDate birth) {
+    this.birth = birth;
+  }
+  public String getGender() {
+    return gender;
+  }
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+  public String getPhonenumber() {
+    return phonenumber;
+  }
+  public void setPhonenumber(String phonenumber) {
+    this.phonenumber = phonenumber;
+  }
+  public String getEmail() {
+    return email;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  public String getAddress() {
+    return address;
+  }
+  public void setAddress(String address) {
+    this.address = address;
+  }
+  public String getTravelConcept() {
+    return travelConcept;
+  }
+  public void setTravelConcept(String travelConcept) {
+    this.travelConcept = travelConcept;
+  }
+  public double getCredit() {
+    return credit;
+  }
+  public void setCredit(double credit) {
+    this.credit = credit;
+  }
+  public boolean isActive() {
+    return active;
+  }
+  public void setActive(boolean active) {
+    this.active = active;
+  }
+  @Override
+  public String toString() {
+    return "Member [id=" + id + ", nickname=" + nickname + ", password=" + password + ", name=" + name + ", birth="
+        + birth + ", gender=" + gender + ", phonenumber=" + phonenumber + ", email=" + email + ", address=" + address
+        + ", travelConcept=" + travelConcept + ", credit=" + credit + ", active=" + active + "]";
+  }
 
-	// 생성자
-	public Member() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public Member(String id, String nickname, String password, String name, LocalDate birth, String gender,
-			String phonenumber, String email, String address, String travelConcept, double credit) {
-		super();
-		this.id = id;
-		this.nickname = nickname;
-		this.password = password;
-		this.name = name;
-		this.birth = birth;
-		this.gender = gender;
-		this.phonenumber = phonenumber;
-		this.email = email;
-		this.address = address;
-		this.travelConcept = travelConcept;
-		this.credit = credit;
-	}
-
-	public Member(String id, String nickname, String password, String phonenumber, String email, String address,
-			String travelConcept) {
-		super();
-		this.id = id;
-		this.nickname = nickname;
-		this.password = password;
-		this.phonenumber = phonenumber;
-		this.email = email;
-		this.address = address;
-		this.travelConcept = travelConcept;
-	}
-
-	// getter setter 메서드
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getNickname() {
-		return nickname;
-	}
-
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public LocalDate getBirth() {
-		return birth;
-	}
-
-	public void setBirth(LocalDate birth) {
-		this.birth = birth;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-
-	public String getPhonenumber() {
-		return phonenumber;
-	}
-
-	public void setPhonenumber(String phonenumber) {
-		this.phonenumber = phonenumber;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getTravelConcept() {
-		return travelConcept;
-	}
-
-	public void setTravelConcept(String travelConcept) {
-		this.travelConcept = travelConcept;
-	}
 	
-	public double getCredit() {
-		return credit;
-	}
-
-	public void setCredit(double credit) {
-		this.credit = credit;
-	}
-
-	@Override
-	public String toString() {
-		return "Member [id=" + id + ", nickname=" + nickname + ", password=" + password + ", name=" + name + ", birth="
-				+ birth + ", gender=" + gender + ", phonenumber=" + phonenumber + ", email=" + email + ", address="
-				+ address + ", travelConcept=" + travelConcept + ", credit=" + credit + "]";
-	}
-
+	
 }
