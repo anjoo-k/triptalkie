163a5595f9cdafc92d452c588534a8bf49573e27
beomjunkim-soul <kbg142337@naver.com>
2025-08-25 03:32:23 +0900

    feat: 별점 기능 추가 - DB 반영 X

---
+++ triptalkie/src/main/java/com/walkietalkie/triptalkie/domain/Star.java
@@ -0,0 +1,73 @@
+package com.walkietalkie.triptalkie.domain;
+
+import java.time.LocalDateTime;
+
+public class Star {
+	private Long idx;
+	private String raterId;
+	private String ratedId;
+	private double rating;
+	private LocalDateTime createdAt;
+	private Long makemateIdx;
+	private boolean israted;
+	
+	public Star() {}
+
+	public Long getIdx() {
+		return idx;
+	}
+
+	public void setIdx(Long idx) {
+		this.idx = idx;
+	}
+
+	public String getRaterId() {
+		return raterId;
+	}
+
+	public void setRaterId(String raterId) {
+		this.raterId = raterId;
+	}
+
+	public String getRatedId() {
+		return ratedId;
+	}
+
+	public void setRatedId(String ratedId) {
+		this.ratedId = ratedId;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Long getMakemateIdx() {
+		return makemateIdx;
+	}
+
+	public void setMakemateIdx(Long makemateIdx) {
+		this.makemateIdx = makemateIdx;
+	}
+
+	public boolean isIsrated() {
+		return israted;
+	}
+
+	public void setIsrated(boolean israted) {
+		this.israted = israted;
+	};
+	
+	
+}
