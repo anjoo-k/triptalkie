f2da05181721c7c3351332c47273cbc8b625881f
GIS <gogointospace@gmail.com>
2025-08-25 22:40:37 +0900

    refactor : 여행 정보 글쓰기 - 나라 선택시 해당되는 도시 자동으로 나오게 개선

--- triptalkie/src/main/resources/static/js/travel-info.js
+++ triptalkie/src/main/resources/static/js/travel-info.js
@@ -1,3 +1,23 @@
 /**
- * 
+ * country 변화 감지해서 city 데이터 가져옴
  */
+document.getElementById("country").addEventListener("change", function() {
+  const countryId = this.value;
+  const citySelect = document.getElementById("city");
+
+  // 기존 옵션 초기화
+  citySelect.innerHTML = '<option value="">-- 도시 선택 --</option>';
+
+  if (countryId) {
+	fetch(`/cities/findCitiesByCountry?countryId=${countryId}`)
+      .then(res => res.json())
+      .then(data => {
+        data.forEach(city => {
+          const option = document.createElement("option");
+          option.value = city.id;
+          option.textContent = city.name;
+          citySelect.appendChild(option);
+        });
+      });
+  }
+});
