c7d293400d5eddf34023345a9e0a89c4ed5201f3
yey3hh <wateveryehwant@gmail.com>
2025-08-26 17:42:01 +0900

    feat: 고객센터 faq 리스트 출력

---
+++ triptalkie/src/main/java/com/walkietalkie/triptalkie/mapper/CustomerserviceMapper.java
@@ -0,0 +1,20 @@
+package com.walkietalkie.triptalkie.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.walkietalkie.triptalkie.domain.Faq;
+
+@Mapper
+public interface CustomerserviceMapper {
+
+	List<Faq> findAllFqa();
+
+	int findCountFaq();
+
+	List<Map<String, Object>> findFaqPaged(@Param("size") int size, @Param("startRow") int startRow);
+
+}
