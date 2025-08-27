a85b9236ef7520a571188a7f4be41677a940e003
yey3hh <wateveryehwant@gmail.com>
2025-08-27 09:30:11 +0900

    feat: faq 기능 추가, 정보 게시판 제목 출력

--- triptalkie/src/main/java/com/walkietalkie/triptalkie/controller/HomeController.java
+++ triptalkie/src/main/java/com/walkietalkie/triptalkie/controller/HomeController.java
@@ -7,10 +7,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.walkietalkie.triptalkie.DTO.TravelReviewTop3DTO;
+import com.walkietalkie.triptalkie.domain.Faq;
 import com.walkietalkie.triptalkie.domain.TravelInfo;
-import com.walkietalkie.triptalkie.domain.TravelReview;
+import com.walkietalkie.triptalkie.service.CustomerserviceService;
 import com.walkietalkie.triptalkie.service.TravelInfoService;
-import com.walkietalkie.triptalkie.service.TravelReviewImageService;
 import com.walkietalkie.triptalkie.service.TravelReviewService;
 
 // Thymeleaf 연습을 위한 컨트롤러
@@ -19,26 +19,27 @@ public class HomeController {
 
 	private final TravelReviewService travelReviewService;
 	private final TravelInfoService travelInfoService;
-//	private final Customer
+	private final CustomerserviceService customerserviceService;
 	
-	public HomeController(TravelReviewService travelReviewService,
-			TravelInfoService travelInfoService) {
+	public HomeController(TravelReviewService travelReviewService, TravelInfoService travelInfoService,
+			CustomerserviceService customerserviceService) {
 		super();
 		this.travelReviewService = travelReviewService;
 		this.travelInfoService = travelInfoService;
+		this.customerserviceService = customerserviceService;
 	}
-
 
 	@GetMapping("/")
 	public String home(Model model) {
 		
 		List<TravelReviewTop3DTO> topReviews = travelReviewService.findTravelreviewTop3();
 		model.addAttribute("topReviews", topReviews);
-		System.out.println("topReviews : " + topReviews);
 		
 		List<TravelInfo> topInfos = travelInfoService.findTravelInfoTop3();
 		model.addAttribute("topInfos", topInfos);
-		System.out.println("topInfos : " + topInfos);
+		
+		List<Faq> faqList = customerserviceService.findFaqTop5();
+		model.addAttribute("faqList", faqList);
 		
 		return "home";
 	}
